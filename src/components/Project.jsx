fe75f01b7498b181f8fa6bae1196b4d33db85b97
unknown <christianjhegggfer@gmail.com>
2023-03-15 00:01:32 +0800

    design fixed

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -4,7 +4,7 @@ const Project = (props) => {
   return (
         <>
             <div>
-              <img src={props.image} width="100%" height="200px" className='rounded'/>
+              <img src={props.image} width="100%" height="300px" className='rounded'/>
               </div>
               <div className='text-center'>
               <h5>{props.name}</h5>
